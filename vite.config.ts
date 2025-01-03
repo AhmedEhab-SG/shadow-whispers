22cd226dab953f74e2c161a5de0812a24eb2ce52
ahmed <ahmedehab.sg@gmail.com>
2025-01-03 14:15:52 +0200

    test manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,69 +1,12 @@
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
+import { ManifestOptions, VitePWA } from "vite-plugin-pwa";
+import manifest from "./public/manifest.json";
 
 export default defineConfig({
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
-      manifest: {
-        short_name: "Shadow Whispers",
-        name: "Shadow Whispers",
-        icons: [
-          {
-            src: "./icon.png",
-            type: "image/png",
-            sizes: "144x144",
-          },
-        ],
-        background_color: "#000000",
-        display: "standalone",
-        scope: "/",
-        start_url: "/",
-        theme_color: "#000000",
-        orientation: "portrait",
-        description:
-          "Shadow Whispers is a game where the player must navigate and kill the monsters and avoid the shadows that lurk within.",
-      },
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,png,svg,woff2}"],
-        runtimeCaching: [
-          {
-            urlPattern: ({ request }) => request.destination === "document",
-            handler: "NetworkFirst",
-            options: {
-              cacheName: "html-cache",
-            },
-          },
-          {
-            urlPattern: ({ request }) => request.destination === "script",
-            handler: "CacheFirst",
-            options: {
-              cacheName: "js-cache",
-            },
-          },
-          {
-            urlPattern: ({ request }) => request.destination === "style",
-            handler: "CacheFirst",
-            options: {
-              cacheName: "css-cache",
-            },
-          },
-          {
-            urlPattern: ({ request }) => request.destination === "image",
-            handler: "CacheFirst",
-            options: {
-              cacheName: "image-cache",
-            },
-          },
-          {
-            urlPattern: ({ request }) => request.destination === "font",
-            handler: "CacheFirst",
-            options: {
-              cacheName: "font-cache",
-            },
-          },
-        ],
-      },
+      manifest: manifest as Partial<ManifestOptions>,
     }),
   ],
 });
