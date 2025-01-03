624ef472922abb8faf16431f9130908d3d5827bd
ahmed <ahmedehab.sg@gmail.com>
2025-01-03 14:02:47 +0200

    adj service worker

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,46 @@ export default defineConfig({
         description:
           "Shadow Whispers is a game where the player must navigate and kill the monsters and avoid the shadows that lurk within.",
       },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,svg,woff2}"],
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === "document",
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "html-cache",
+            },
+          },
+          {
+            urlPattern: ({ request }) => request.destination === "script",
+            handler: "CacheFirst",
+            options: {
+              cacheName: "js-cache",
+            },
+          },
+          {
+            urlPattern: ({ request }) => request.destination === "style",
+            handler: "CacheFirst",
+            options: {
+              cacheName: "css-cache",
+            },
+          },
+          {
+            urlPattern: ({ request }) => request.destination === "image",
+            handler: "CacheFirst",
+            options: {
+              cacheName: "image-cache",
+            },
+          },
+          {
+            urlPattern: ({ request }) => request.destination === "font",
+            handler: "CacheFirst",
+            options: {
+              cacheName: "font-cache",
+            },
+          },
+        ],
+      },
     }),
   ],
 });
